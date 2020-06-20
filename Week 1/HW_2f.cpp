49bf564e3e0728cec9ebb9dc51d7ea76e7cc8bf8
Zachary Meyer <38173430+itiszac@users.noreply.github.com>
2020-06-20 02:57:10 -0700

    Add files via upload

---
+++ Week 1/HW_2f.cpp
@@ -0,0 +1,42 @@
+// Attached: HW_2f
+// File: HW_2f.cpp
+//===============================================================================
+//
+// Programmer: Zachary Meyer
+// Class: CS 1A
+// Instructor: Med Mogasemi
+//
+//==============================================================================
+// Program: Checking Character data
+//==============================================================================
+// Description:
+//			An output from the console that prints,
+//			a birthday cake made from characters on the keyboard,
+//==============================================================================
+//==============================================================================
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "     ,,,,," << std::endl;
+	std::cout << "    _!!!!!_" << std::endl;
+	std::cout << "   {~*~*~*~}" << std::endl;
+	std::cout << " __{*~*~*~*}__" << std::endl;
+	std::cout << ";             ;" << std::endl;
+	std::cout << "---------------" << std::endl;
+
+	system("pause");
+	return 0;
+}
+
+/*
+=============Output===============================
+	 ,,,,,
+	_!!!!!_
+   {~*~*~*~}
+ __{*~*~*~*}__
+;             ;
+---------------
+Press any key to continue . . .
+*/
