d1b605c89c3fae40443a5ee352ac7c142058abd0
Zachary Meyer <38173430+itiszac@users.noreply.github.com>
2020-06-20 02:48:26 -0700

    Create HW_2b.cpp

---
+++ HW_2b.cpp
@@ -0,0 +1,34 @@
+// Attached: HW_2b
+// File: HW_2b.cpp
+//===============================================================================
+//
+// Programmer: Zachary Meyer
+// Class: CS 1A
+// Instructor: Med Mogasemi
+//
+//==============================================================================
+// Program: Checking Character data
+//==============================================================================
+// Description:
+//			An output from the console that says,
+//			"My age is x!"
+//==============================================================================
+//==============================================================================
+
+#include <iostream>
+
+int main()
+{
+	int myAge = 26;
+
+	std::cout << "My age is " << myAge << std::endl;
+
+	system("pause");
+	return 0;
+}
+
+/*
+=============Output===============================
+My age is 26
+Press any key to continue . . .
+*/
